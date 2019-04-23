388bd79e681dae7f6a1200e7a585f8c18b9d1701
LukasKrocek <Krocek.Lukas@email.cz>
2019-04-23 22:19:56 +0200

    making final changes in gardenApp

--- week-04/day-1/src/thegardenapp/Garden.java
+++ week-04/day-1/src/thegardenapp/Garden.java
@@ -4,18 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Garden {
-    List<Plant> treesAndFlowers = new ArrayList<>();
+    private List<Plant> treesAndFlowers = new ArrayList<>();
+
+    public void add (Plant plant){
+        treesAndFlowers.add(plant);
+    }
 
     public void watering(int amountOfwater) {
         System.out.println("Watering with " + amountOfwater);
-        int waterPerPlant = amountOfwater / getNumberOfThirsty();//dividing water between thirsty plants
         for (Integer index : getThirstyIndexes()) { //watering thirsty plants
-            treesAndFlowers.get(index).watering(waterPerPlant);
+            treesAndFlowers.get(index).watering(divideWaterPerPlant(amountOfwater));
         }
-        printNeeds();
     }
 
-    public void printNeeds() {
+    private int divideWaterPerPlant(int amountOfwater) {
+        return amountOfwater / getNumberOfThirsty();
+    }
+
+    public void printStatus() {
         for (Plant plant : treesAndFlowers) {
             if (plant.setNeedsWater()) {
                 System.out.println(plant.color + " " + plant.getClass().getSimpleName() + " needs water");
--- week-04/day-1/src/thegardenapp/Main.java
+++ week-04/day-1/src/thegardenapp/Main.java
@@ -1,33 +1,22 @@
 package thegardenapp;
 
 public class Main {
-    /*
-    The task is to create a garden application, so in your main method you should create a garden with
-     flowers and trees. The program should demonstrate an example garden with two flowers (yellow and blue)
-      and two trees (purple and orange). In the example after creating them you should show the user, how the garden
-       looks like. After that the program should water the garden twice, first with the amount of 40 then with 70.
-        And after every watering the user should see the state of the garden as you can see in the output.
-     */
-
-
-
     public static void main(String[] args) {
         Garden garden = new Garden();
         Flower yellowFlower = new Flower("yellow");
         Flower blueFlower = new Flower("blue");
-        Tree purpleTree = new Tree ("purple");
-        Tree orangeTree = new Tree ("orange");
+        Tree purpleTree = new Tree("purple");
+        Tree orangeTree = new Tree("orange");
 
-        garden.treesAndFlowers.add(yellowFlower);
-        garden.treesAndFlowers.add(blueFlower);
-        garden.treesAndFlowers.add(purpleTree);
-        garden.treesAndFlowers.add(orangeTree);
-
+        garden.add(yellowFlower);
+        garden.add(blueFlower);
+        garden.add(purpleTree);
+        garden.add(orangeTree);
+        garden.printStatus();
         garden.watering(40);
+        garden.printStatus();
         garden.watering(70);
-
-
-
+        garden.printStatus();
 
 
     }
