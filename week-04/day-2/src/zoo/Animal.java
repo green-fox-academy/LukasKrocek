215dca913877088e15a5cad6d077e67540d4910f
LukasKrocek <Krocek.Lukas@email.cz>
2019-04-24 13:27:40 +0200

    Zoo exercise finished

---
+++ week-04/day-2/src/zoo/Animal.java
@@ -0,0 +1,24 @@
+package zoo;
+
+public abstract class Animal {
+    /*
+it has getName() and breed() methods
+*/
+
+    private String name;
+    int age;
+    String gender;
+    int numberOfLegs;
+    boolean hungry;
+    boolean happy;
+
+    public Animal (String name){
+        this.name = name;
+    }
+
+    public String getName (){
+        return name;
+    }
+
+    public abstract String breed ();
+}
