2f32c3f450c6165abc6d7d70483c40259089a374
LukasKrocek <Krocek.Lukas@email.cz>
2019-04-25 14:47:25 +0200

    finished StringCounterTest

--- week-04/day-3/src/main/java/StringCounter.java
+++ week-04/day-3/src/main/java/StringCounter.java
@@ -1,18 +1,23 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class StringCounter {
 
-    public HashMap<String, Integer> stringCounter(String text) {
+    public HashMap<String, Integer> countWords(String text) {
         if (text.length() == 0) {
             return null;
         }
-
         HashMap<String, Integer> counter = new HashMap<>();
-        String[] words = text.split(" ");
-
-        if (words.length == 0) {
+        text = text.trim();
+        while (text.contains("  ")) { //getting rid off additional whitespaces
+            text = text.replaceAll("  ", " ");
+        }
+        if (text.equals("")) { //only whitespace, returns null
             return null;
         }
+
+        String[] words = text.split(" ");
+
         for (String word : words) {
             if (counter.containsKey(word)) {
                 counter.put(word, counter.get(word) + 1);
@@ -20,7 +25,6 @@ public class StringCounter {
                 counter.put(word, 1);
             }
         }
-
         return counter;
     }
 }
--- week-04/day-3/src/test/java/StringCounterTest.java
+++ week-04/day-3/src/test/java/StringCounterTest.java
@@ -1,10 +1,53 @@
 import org.junit.Test;
 
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
 public class StringCounterTest {
 
 
     @Test
+    public void countWords_InNormalText_returnsCountOfWords() {
+        StringCounter counter = new StringCounter();
+        String text = "Ho Ho Ho guys Ho";
+        HashMap<String, Integer> checkMap = new HashMap<>();
+        checkMap.put("Ho", 4);
+        checkMap.put("guys", 1);
+        assertEquals(checkMap, counter.countWords(text));
+    }
 
+    @Test
+    public void countWords_InTextWithWhiteSpaces_returnsCountOfWords() {
+        StringCounter counter = new StringCounter();
+        String text = "    Ho     Ho      Ho      guys Ho        ";
+        HashMap<String, Integer> checkMap = new HashMap<>();
+        checkMap.put("Ho", 4);
+        checkMap.put("guys", 1);
+        assertEquals(checkMap, counter.countWords(text));
+    }
 
+    @Test
+    public void countWords_InEmptyStrings_returnsNull() {
+        StringCounter counter = new StringCounter();
+        String text = "";
+        assertEquals(null, counter.countWords(text));
+    }
+
+    @Test
+    public void countWords_InOnlyWhiteSpaceStrings_returnsNull() {
+        StringCounter counter = new StringCounter();
+        String text = "             ";
+        assertEquals(null, counter.countWords(text));
+    }
+
+    @Test
+    public void countWords_InOneWordString_returnsCountOfWord() {
+        StringCounter counter = new StringCounter();
+        String text = "Ho";
+        HashMap<String, Integer> checkMap = new HashMap<>();
+        checkMap.put("Ho", 1);
+        assertEquals(checkMap, counter.countWords(text));
+    }
 
 }
