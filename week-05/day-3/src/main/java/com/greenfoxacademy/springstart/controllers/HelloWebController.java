847c69e7aa9889794cc10831a9c3177f3dd5ee4a
LukasKrocek <Krocek.Lukas@email.cz>
2019-05-15 16:04:25 +0200

    final changes in greeting app

--- week-05/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-05/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -11,15 +11,23 @@ public class HelloWebController {
     public String greeting(Model model, @RequestParam String name) {
         model.addAttribute("greeting", Greetings.getRandomHello());
         model.addAttribute("name", name); //color: rgb(0,0,255);
-        model.addAttribute("style",
-                "color: rgb("
-                        + (int) (Math.random() * 256) + "," + (int) (Math.random() * 256) + ","
-                        + (int) (Math.random() * 256) + ");" +
-                            "font-size:"
-                        + (int) (Math.random() * 100 + 12) + "px;");
-        model.addAttribute("fontSize", "font-size:40px;");
+        model.addAttribute("style",getRandomRGBString()+getRandomFontSizeString());
         model.addAttribute("id", Greetings.incrementAndGetID());
         return "greeting";
     }
+
+    public String getRandomRGBString() {
+        return "color: rgb("
+                + (int) (Math.random() * 256) + ","
+                + (int) (Math.random() * 256) + ","
+                + (int) (Math.random() * 256) + ");";
+    }
+
+    public String getRandomFontSizeString() {
+        return "font-size:"
+                + (int) (Math.random() * 100 + 12) + "px;";
+    }
 }
+
+
 
