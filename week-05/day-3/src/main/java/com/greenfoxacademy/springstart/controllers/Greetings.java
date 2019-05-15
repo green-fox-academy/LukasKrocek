45516a3f8b63f0d7247fed9f1c3cbfd599d4a222
LukasKrocek <Krocek.Lukas@email.cz>
2019-05-15 15:50:07 +0200

    adding last exercise - thymeleaf changing colors and fonts

--- week-05/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/Greetings.java
+++ week-05/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/Greetings.java
@@ -1,23 +1,35 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Greetings {
 
     private static AtomicLong id = new AtomicLong();
     private String content;
+    private static String language;
 
     public Greetings(String content) {
         this.content = content;
         id.incrementAndGet();
     }
+
     public AtomicLong getId() {
         return id;
     }
 
-    public static Long incrementAndGet (){
+    public static Long incrementAndGetID() {
         return id.incrementAndGet();
 
+    }
+
+    public static String getRandomHello() {
+        String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+                "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+                "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+                "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+        int indexOFHello = (int) (Math.random() * hellos.length);
+        return hellos[indexOFHello];
     }
 
     public String getContent() {
--- week-05/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-05/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -9,8 +9,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWebController {
     @RequestMapping("/web/greeting")
     public String greeting(Model model, @RequestParam String name) {
-        model.addAttribute("name", name);
-        model.addAttribute("id", Greetings.incrementAndGet());
+        model.addAttribute("greeting", Greetings.getRandomHello());
+        model.addAttribute("name", name); //color: rgb(0,0,255);
+        model.addAttribute("style",
+                "color: rgb("
+                        + (int) (Math.random() * 256) + "," + (int) (Math.random() * 256) + ","
+                        + (int) (Math.random() * 256) + ");" +
+                            "font-size:"
+                        + (int) (Math.random() * 100 + 12) + "px;");
+        model.addAttribute("fontSize", "font-size:40px;");
+        model.addAttribute("id", Greetings.incrementAndGetID());
         return "greeting";
     }
 }
